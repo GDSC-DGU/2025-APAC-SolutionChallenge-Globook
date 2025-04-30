af034642cae2431644b9098f5ee80c8cdc873ad1
홍규진 <hkj0206@dgu.ac.kr>
2025-04-30 19:30:06 +0900

    feat: Implement Book Domain model

---
+++ globook_client/lib/domain/model/book.dart
@@ -0,0 +1,25 @@
+class Book {
+  final String id;
+  final String title;
+  final String author;
+  final String imageUrl;
+  final String description;
+  final String category;
+  final List<Book> authorBooks;
+
+  const Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.imageUrl,
+    required this.description,
+    required this.category,
+    this.authorBooks = const [],
+  });
+}
+
+enum BookCategory {
+  fiction,
+  nonFiction,
+  philosophy,
+}
