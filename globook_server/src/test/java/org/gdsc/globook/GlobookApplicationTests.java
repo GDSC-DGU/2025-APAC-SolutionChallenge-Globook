109618fe2f98a2b89ba2df97fcf9407ec516eba5
Jang99u <alswkdrb@dgu.ac.kr>
2025-05-04 22:25:39 +0900

    feat: Initialize spring project

---
+++ globook_server/src/test/java/org/gdsc/globook/GlobookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdsc.globook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
