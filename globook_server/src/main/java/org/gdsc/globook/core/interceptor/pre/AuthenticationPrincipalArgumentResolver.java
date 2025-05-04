0cf8782665a1fbff629296dca23d51bb97850f4c
jun6292 <c68254@gmail.com>
2025-05-05 03:32:38 +0900

    feat: Implement Custom Annotation, Interceptor & ArgumentResolver

---
+++ globook_server/src/main/java/org/gdsc/globook/core/interceptor/pre/AuthenticationPrincipalArgumentResolver.java
@@ -0,0 +1,37 @@
+package org.gdsc.globook.core.interceptor.pre;
+
+import lombok.extern.slf4j.Slf4j;
+import org.gdsc.globook.core.annotation.AuthenticationPrincipal;
+import org.gdsc.globook.core.exception.CustomException;
+import org.gdsc.globook.core.exception.GlobalErrorCode;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@Slf4j
+public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        log.info("supportsParameter = {}", parameter.getParameterType().equals(Long.class) &&
+                parameter.hasParameterAnnotation(AuthenticationPrincipal.class));
+        return parameter.getParameterType().equals(Long.class) && parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        final Object userIdObj = webRequest.getAttribute("AUTHENTICATION_PRINCIPAL", NativeWebRequest.SCOPE_REQUEST);
+
+        log.info("resolveArgument = {}", userIdObj);
+        if (userIdObj == null) {
+            throw new CustomException(GlobalErrorCode.ACCESS_DENIED_ERROR);
+        }
+        return Long.valueOf(userIdObj.toString());
+    }
+}
