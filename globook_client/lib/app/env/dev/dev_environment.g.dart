ad324a69803c5a7a6ace5fca40806c4de9e153b2
홍규진 <hkj0206@dgu.ac.kr>
2025-05-05 21:18:35 +0900

    feat: Implement Environment by envied_generator -2

---
+++ globook_client/lib/app/env/dev/dev_environment.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dev_environment.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// generated_from: assets/config/.dev.env
+final class _DevEnvironment {}
