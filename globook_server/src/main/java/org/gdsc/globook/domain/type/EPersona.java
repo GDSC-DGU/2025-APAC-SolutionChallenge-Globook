82db6bd4a8679d60e16148bef9ec5205c92877b0
Jang99u <alswkdrb@dgu.ac.kr>
2025-05-11 22:58:36 +0900

    feat: Enhance TTS persona

--- globook_server/src/main/java/org/gdsc/globook/domain/type/EPersona.java
+++ globook_server/src/main/java/org/gdsc/globook/domain/type/EPersona.java
@@ -6,12 +6,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum EPersona {
-    ETHAN("ETHAN", "MALE", 1.0, 1.0),
-    LUNA("LUNA", "FEMALE", 1.0, 1.0),
-    KAI("KAI", "MALE", 1.0, 1.0),
-    SORA("SORA", "FEMALE", 1.0, 1.0),
+    ETHAN("ETHAN", "MALE", 0.7, 0.7),
+    LUNA("LUNA", "FEMALE", 1.3, 1.0),
+    KAI("KAI", "MALE", 1.3, 1.3),
+    SORA("SORA", "FEMALE", 1.0, 1.3),
     NOAH("NOAH", "MALE", 1.0, 1.0),
-    ARIA("ARIA", "FEMALE", 1.0, 1.0);
+    ARIA("ARIA", "FEMALE", 0.7, 0.7);
 
     private final String persona;
     private final String gender;
