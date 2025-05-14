e89a281677dad886981cb9632635b55ef4751478
홍규진 <hkj0206@dgu.ac.kr>
2025-05-14 16:54:19 +0900

    feat: Implement changed Routing

--- globook_client/lib/app/config/app_routes.dart
+++ globook_client/lib/app/config/app_routes.dart
@@ -6,9 +6,11 @@ abstract class AppRoutes {
   static const String ROOT = '/root';
   static const String SPLASH = '/splash';
   static const String HOME = '/home';
+  static const String STORAGE = '/storage';
   static const String BOOK_STORE = '/book-store';
   static const String BOOK_STORE_DETAIL = '/book-store-detail/:bookId';
   static const String GENRE_BOOKS = '/genre-books/:genreId';
-  static const String READER = '/reader/:bookId';
+  static const String READER = '/reader/:fileId/:index/:type';
   static const String FAVORITE = '/favorite';
+  static const String UPLOAD = '/upload';
 }
