ccc74d9bfab4694a71df1b7a8e5546ddbc96ab03
홍규진 <hkj0206@dgu.ac.kr>
2025-05-14 18:48:41 +0900

    feat: Implement Reader Service Provider Layer

--- globook_client/lib/data/provider/reader/reader_provider_impl.dart
+++ globook_client/lib/data/provider/reader/reader_provider_impl.dart
@@ -1,3 +1,4 @@
+import 'package:globook_client/app/utility/log_util.dart';
 import 'package:globook_client/core/provider/base_connect.dart';
 import 'package:globook_client/data/model/repsonse_wrapper.dart';
 import 'package:globook_client/data/provider/reader/reader_provider.dart';
@@ -5,26 +6,57 @@ import 'package:globook_client/domain/model/reader.dart';
 
 class ReaderProviderImpl extends BaseConnect implements ReaderProvider {
   @override
-  Future<ResponseWrapper<TTSMDText>> getTTSMDTextFirst(
-      String bookId, int index) async {
+  Future<ResponseWrapper<ReaderResponse>> getTTSMDTextFirst(
+      bool isFile, int fileId, int index) async {
+    final inputType = isFile ? 'FILE' : 'BOOK';
+    final response = await get(
+        '/api/v1/users/paragraphs?type=$inputType&fileId=$fileId&index=$index&direction=FIRST',
+        headers: BaseConnect.usedAuthorization);
+    LogUtil.info(response.body);
+    if (response.statusCode == 200 && response.body['success'] == true) {
+      final data = response.body['data'] as Map<String, dynamic>;
+      final readerResponse = ReaderResponse.fromJson(data);
+      return ResponseWrapper(success: true, data: readerResponse);
+    } else {
+      handleError(response.body['message']);
+    }
     return ResponseWrapper(
-        success: true,
-        data: TTSMDText(index: index, text: 'Hello', voiceFile: 'Hello'));
+        success: false, data: null, message: response.body['message']);
   }
 
   @override
-  Future<ResponseWrapper<TTSMDText>> getTTSMDTextAfter(
-      String bookId, int index) async {
+  Future<ResponseWrapper<ReaderResponse>> getTTSMDTextAfter(
+      bool isFile, int fileId, int index) async {
+    final inputType = isFile ? 'FILE' : 'BOOK';
+    final response = await get(
+        '/api/v1/users/paragraphs?type=$inputType&fileId=$fileId&index=$index&direction=DOWN',
+        headers: BaseConnect.usedAuthorization);
+    if (response.statusCode == 200 && response.body['success'] == true) {
+      final data = response.body['data'] as Map<String, dynamic>;
+      final readerResponse = ReaderResponse.fromJson(data);
+      return ResponseWrapper(success: true, data: readerResponse);
+    } else {
+      handleError(response.body['message']);
+    }
     return ResponseWrapper(
-        success: true,
-        data: TTSMDText(index: index, text: 'Hello', voiceFile: 'Hello'));
+        success: false, data: null, message: response.body['message']);
   }
 
   @override
-  Future<ResponseWrapper<TTSMDText>> getTTSMDTextBefore(
-      String bookId, int index) async {
+  Future<ResponseWrapper<ReaderResponse>> getTTSMDTextBefore(
+      bool isFile, int fileId, int index) async {
+    final inputType = isFile ? 'FILE' : 'BOOK';
+    final response = await get(
+        '/api/v1/users/paragraphs?type=$inputType&fileId=$fileId&index=$index&direction=UP',
+        headers: BaseConnect.usedAuthorization);
+    if (response.statusCode == 200 && response.body['success'] == true) {
+      final data = response.body['data'] as Map<String, dynamic>;
+      final readerResponse = ReaderResponse.fromJson(data);
+      return ResponseWrapper(success: true, data: readerResponse);
+    } else {
+      handleError(response.body['message']);
+    }
     return ResponseWrapper(
-        success: true,
-        data: TTSMDText(index: index, text: 'Hello', voiceFile: 'Hello'));
+        success: false, data: null, message: response.body['message']);
   }
 }
